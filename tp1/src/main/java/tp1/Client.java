689f0a7e681640cae846e528adef6ff7f84d662c
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2021-01-21 20:00:41 -0500

    tp1 template

---
+++ tp1/src/main/java/tp1/Client.java
@@ -0,0 +1,7 @@
+package tp1;
+
+public class Client
+{
+    public static void main( String[] args )
+    {}
+}
---
+++ tp1/src/main/java/tp1/Server.java
@@ -0,0 +1,7 @@
+package tp1;
+
+public class Server
+{
+    public static void main( String[] args )
+    {}
+}
