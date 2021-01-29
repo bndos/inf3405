f91f3a8b9180720a62e004749db9d9c0e52e24b1
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2021-01-28 22:14:12 -0500

    added user input validation

--- tp1/src/main/java/tp1/Client.java
+++ tp1/src/main/java/tp1/Client.java
@@ -2,15 +2,30 @@ package tp1;
 
 import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Client
-{
+public class Client {
     private static Socket socket;
 
-    public static void main( String[] args ) throws Exception
-    {
-        String serverAddress = "localhost";
-        int port = 5000;
+    public static void main(String[] args) throws Exception {
+        InputParser inputParser = new InputParser();
+        String serverAddress;
+        int port;
+
+        try {
+            serverAddress = inputParser.getIP();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        try {
+            port = inputParser.getPort();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
 
         socket = new Socket(serverAddress, port);
 
@@ -22,5 +37,53 @@ public class Client
         System.out.println(helloMessageFromServer);
 
         socket.close();
+    }
+
+    public static class InputParser {
+        private Scanner scanner;
+
+        public InputParser() {
+            scanner = new Scanner(System.in);
+        }
+
+        private int parseInt(String s, int minValue, int maxValue, String error) throws Exception {
+            int val;
+            try {
+                val = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new Exception(error);
+            }
+
+            if (val < minValue || val > maxValue)
+                throw new Exception(error);
+
+            return val;
+        }
+
+        public String getIP() throws Exception {
+            System.out.print("Server address: ");
+
+            String ip = scanner.next();
+            String[] bytes = ip.split("\\.", -1);
+            String error = "IP address must be in the format '0-255.0-255.0-255.0-255'";
+
+            if (bytes.length != 4)
+                throw new Exception(error);
+
+            for (String value : bytes) {
+                parseInt(value, 0, 255, error);
+            }
+
+            return ip;
+        }
+
+        public int getPort() throws Exception {
+            System.out.print("Server port: ");
+
+            String error = "The port should be a number between 5000 and 5050";
+            int port = parseInt(scanner.next(), 5000, 5050, error);
+
+            return port;
+        }
     }
 }
--- tp1/src/main/java/tp1/Server.java
+++ tp1/src/main/java/tp1/Server.java
@@ -7,12 +7,10 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server
-{
+public class Server {
     private static ServerSocket listener;
 
-    public static void main( String[] args ) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         int clientNumber = 0;
 
         String serverAddress = "localhost";
@@ -30,44 +28,35 @@ public class Server
             while (true) {
                 new ClientHandler(listener.accept(), clientNumber++).start();
             }
-        }
-        finally {
+        } finally {
             listener.close();
         }
     }
 
-    /**
-     * InnerServer
-     */
-    public static class ClientHandler extends Thread
-    {
+    public static class ClientHandler extends Thread {
         private Socket socket;
         private int clientNumber;
 
-        public ClientHandler(Socket socket, int clientNumber)
-        {
+        public ClientHandler(Socket socket, int clientNumber) {
             this.socket = socket;
             this.clientNumber = clientNumber;
             System.out.println("New connection with client#" + clientNumber + " at " + socket);
         }
 
-        public void run()
-        {
+        public void run() {
             try {
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 out.writeUTF("Hello from server - you are client #" + clientNumber);
-            } catch(IOException e) {
+            } catch (IOException e) {
                 System.out.println("Error handling client #" + clientNumber + ": " + e);
-            }
-            finally {
+            } finally {
                 try {
                     socket.close();
-                } catch(IOException e) {
+                } catch (IOException e) {
                     System.out.println("Couldn't close a scoket, what's gouing on?");
                 }
                 System.out.println("Connection with client #" + clientNumber + " closed");
             }
         }
-
     }
 }
