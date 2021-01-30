594168fd2c55bfd52bcbda7d694d669a9a4fbc16
Brando Tovar <grover-brando.tovar-oblitas@polymtl.ca>
2021-01-30 02:57:17 -0500

    added cd and mkdir implementation

--- tp1/src/main/java/tp1/Api.java
+++ tp1/src/main/java/tp1/Api.java
@@ -2,12 +2,14 @@ package tp1;
 
 import java.io.File;
 import java.net.Socket;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.function.Function;
 
 public class Api {
-    private File cwd;
-    private Socket socket;
+    private static File cwd = new File(".");
+    private static Socket socket;
 
     public static enum Command {
         CD,
@@ -26,132 +28,71 @@ public class Api {
         }
     }
 
-    private void changeDir(String path) {
-        this.cwd = new File(path);
-    }
-
-    private abstract class Operation {
-        public File currentDirectory;
-
-        Operation(File dir) {
-            System.out.println(dir == null);
-            this.currentDirectory = dir;
-        }
-
-        public String execute(String[] args) {
+    static private Function<String[], String> cd = (args) -> {
+        if (args == null || args.length > 2 || args.length < 2)
             return "";
-        }
-    }
 
-    private class cd extends Operation {
-        cd(File dir) {
-            super(dir);
-        }
+        File nextDir = new File(Paths.get(cwd.getAbsolutePath()).normalize() + "/" + args[1]);
+        if (!nextDir.exists())
+            return args[0] + ": No such file or directory: " + args[1];
+        if (!nextDir.isDirectory())
+            return args[0] + ": Not a directory: " + args[1];
 
-        public String execute(String[] args) {
-            if (args == null || args.length > 2 || args.length < 2)
-                return "";
+        cwd = nextDir;
 
-            File nextDir = new File(args[1]);
-            System.out.println(nextDir.getAbsolutePath());
-            if (!nextDir.exists())
-                return args[0] + ": No such file or directory: " + args[1];
-            if (!nextDir.isDirectory())
-                return args[0] + ": Not a directory: " + args[1];
+        return "";
+    };
 
-            this.currentDirectory = new File(nextDir.getAbsolutePath());
-
-            return cwd.getAbsolutePath();
-        }
-    }
-
-    private class ls extends Operation {
-        ls(File dir) {
-            super(dir);
+    static private Function<String[], String> ls = (args) -> {
+        String files = "";
+        for (String file : cwd.list()) {
+            files += file + " ";
         }
 
-        public String execute(String[] args) {
-            String files = "";
-            for (String file : this.currentDirectory.list()) {
-                files += file + " ";
-            }
+        return files;
+    };
 
-            System.out.println(cwd.getAbsolutePath());
-
-            return files;
-        }
-    }
-    private class mkdir extends Operation {
-        mkdir(File dir) {
-            super(dir);
-        }
-
-        public String execute(String[] args) {
-            int i = 0;
-            for (String dir : args) {
-                if (i++ != 0) {
-                    File dirFile = new File(dir);
-                    if (!dirFile.mkdir())
-                        return "Couldn't make dir: " + dir;
-                }
-            }
-
-            return "";
-        }
-    }
-
-    private class upload extends Operation {
-        upload(File dir) {
-            super(dir);
-        }
-
-        public String execute(String[] args) {
-            for (String dir : args) {
-                File absoluteFile = new File(cwd.getAbsoluteFile() + dir);
-                if (!absoluteFile.mkdir())
+    static private Function<String[], String> mkdir = (args) -> {
+        int i = 0;
+        for (String dir : args) {
+            if (i++ != 0) {
+                File dirFile = new File(dir);
+                if (!dirFile.mkdir())
                     return "Couldn't make dir: " + dir;
             }
-
-            return "";
-        }
-    }
-
-    private class download extends Operation {
-        download(File dir) {
-            super(dir);
         }
 
-        public String execute(String[] args) {
-            String files = "";
-            for (String file : cwd.list()) {
-                files += file + " ";
-            }
+        return "";
+    };
 
-            return files;
-        }
-    }
-
-    private class exit extends Operation {
-        exit(File dir) {
-            super(dir);
+    static private Function<String[], String> upload = (args) -> {
+        for (String dir : args) {
+            File absoluteFile = new File(cwd.getAbsoluteFile() + dir);
+            if (!absoluteFile.mkdir())
+                return "Couldn't make dir: " + dir;
         }
 
-        public String execute(String[] args) {
-            return "";
+        return "";
+    };
+
+    static private Function<String[], String> download = (args) -> {
+        String files = "";
+        for (String file : cwd.list()) {
+            files += file + " ";
         }
-    }
 
-    private ArrayList<Operation> commands =
-        new ArrayList<Operation>(Arrays.asList(new cd(this.cwd),
-                                               new ls(this.cwd),
-                                               new mkdir(this.cwd),
-                                               new upload(this.cwd),
-                                               new download(this.cwd),
-                                               new exit(this.cwd)));
+        return files;
+    };
 
+    static private Function<String[], String> exit = (args) -> {
+        return "";
+    };
+
+    private static ArrayList<Function<String[], String>> commands;
     public Api(Socket s) {
-        this.cwd    = new File(".");
-        this.socket = s;
+        socket   = s;
+        commands = new ArrayList<Function<String[], String>>(
+            Arrays.asList(cd, ls, mkdir, upload, download, exit));
     }
 
     public String exec(String[] args) {
@@ -159,6 +100,6 @@ public class Api {
             return "";
 
         int commandIndex = Command.getCmd(args[0]).ordinal();
-        return commands.get(commandIndex).execute(args);
+        return commands.get(commandIndex).apply(args);
     }
 }
