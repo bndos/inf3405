1ac706c6591b2a43cc6d05cec09636ffdf3ea46e
Pritam <pritapat@gmail.com>
2021-02-02 15:46:47 -0500

    initial project

--- tp1/src/main/java/tp1/Client.java
+++ tp1/src/main/java/tp1/Client.java
@@ -1,7 +1,31 @@
 package tp1;
 
+import java.io.DataInputStream;
+import java.net.Socket;
+
 public class Client
-{
-    public static void main( String[] args )
-    {}
+{   
+    private static Socket socket;
+
+    /* Application client*/
+    public static void main( String[] args ) throws Exception { 
+       // Adresse et port du serveur
+       String serverAddress = "127.0.0.1";
+       int port = 5000;
+
+       // Création d'une nouvelle connection avec le serveur
+       socket = new Socket(serverAddress, port);
+
+       System.out.format("The server is running on %s:%d%n", serverAddress, port);
+        
+       // Création d'un canal entrant pour recevoir les messages 
+       DataInputStream in = new DataInputStream(socket.getInputStream());
+
+       // Attente de la reception d'un message envoye par le serveur sur le canal
+       String helloMessageFromServer = in.readUTF();
+       System.out.println(helloMessageFromServer);
+
+       // Fermeture de la connexion avec le serveur
+       socket.close();
+    }
 }
