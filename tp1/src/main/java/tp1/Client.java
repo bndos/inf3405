2c2427e53c1e12e58182b24e5ebd13a3c573fc2a
Pritam <pritapat@gmail.com>
2021-02-04 19:21:13 -0500

    added address demand and input validation

--- tp1/src/main/java/tp1/Client.java
+++ tp1/src/main/java/tp1/Client.java
@@ -2,21 +2,25 @@ package tp1;
 
 import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client
 {   
+    private static String clientIP = "";
+    private static int clientPort = 0;
+    private static Scanner clientInput = new Scanner(System.in); // Creation du Scanner pour avoir le user input
     private static Socket socket;
 
     /* Application client*/
-    public static void main( String[] args ) throws Exception { 
-       // Adresse et port du serveur
-       String serverAddress = "127.0.0.1";
-       int port = 5000;
+    public static void main( String[] args ) throws Exception {
+
+        // Tant que l'addresse et le port ne sont pas valides demander de nouveau
+        while(invalidInput());
 
        // Création d'une nouvelle connection avec le serveur
-       socket = new Socket(serverAddress, port);
+       socket = new Socket(clientIP, clientPort);
 
-       System.out.format("The server is running on %s:%d%n", serverAddress, port);
+       System.out.format("The server is running on %s:%d%n", clientIP, clientPort);
         
        // Création d'un canal entrant pour recevoir les messages 
        DataInputStream in = new DataInputStream(socket.getInputStream());
@@ -28,4 +32,93 @@ public class Client
        // Fermeture de la connexion avec le serveur
        socket.close();
     }
+
+        /*
+    * Demander le IP addresse et le port, puis verifier s'il sont valides
+    */
+    private static Boolean invalidInput() {   
+        // Demander au client sur quel port il veut se connecter
+        System.out.println("Please enter the IP address and the port (between 5000 and 5050) to connect to: ");
+        String input = clientInput.nextLine();
+        // Decouper le input de maniere que A.B.C.D:E -> [A, B, C, D, E]
+        String[] inputArray = input.split("[:\\.]", 6);
+
+        return(validInput(inputArray) && validIP(inputArray) && validPort(inputArray));
+    }
+
+    /*
+    * Verifier si le input addresse est valide
+    */
+    private static Boolean validInput(String[] inputArray) {
+        boolean validInput = true;
+
+        if(inputArray.length != 5) {
+            validInput = false;
+            System.out.println(" Please enter a valid input! (eg. 127.0.0.1:5001)");
+        }
+    
+        return(validInput);
+
+    }
+
+    /*
+    * Verifier si le IP addresse est valide
+    */
+    private static Boolean validIP(String[] inputArray) {
+        boolean validIP = true;
+        int ipSize = 4; // 4 octets
+        for(int i = 0; i < ipSize; i++) {
+            
+            int octet = 0;
+            
+            // Verifier si ce n'est pas des lettres
+            try {
+                octet = Integer.parseInt(inputArray[i]);
+            }
+            catch(NumberFormatException e) {
+                validIP = false;
+                System.out.println(" Please enter a valid IP number! (no letters)");
+            }
+
+            // Verifier si respecte [0, 255]
+            if(octet < 0 || octet > 255) {
+                validIP = false;
+                System.out.println(" Please enter an IP address inside bounderies! (XX.XX.XX.XX:YYYY where XX = [0, 255])");
+            }
+            
+            clientIP = inputArray[i] + '.';
+        }
+        return validIP;
+    }
+
+    /*
+    * Verifier si le port est valide
+    */
+    private static Boolean validPort(String[] inputArray) {
+
+        boolean validPort = true;
+        String portInput = inputArray[5];
+            
+            int port = 0;
+            
+            // Verifier si ce n'est pas des lettres
+            try {
+                port = Integer.parseInt(portInput);
+            }
+            catch(NumberFormatException e) {
+                validPort = false;
+                System.out.println(" Please enter a valid port number! (no letters)");
+            }
+
+            // Verifier si respecte [0, 255]
+            if(port < 5000 || port > 5050) {
+                validPort = false;
+                System.out.println(" Please enter a port number inside bounderies! (XX.XX.XX.XX:YYYY where YYYY = [5000, 5050])");
+            }
+            
+            clientPort = port;
+
+        return validPort;
+    }
 }
+
--- tp1/src/main/java/tp1/Server.java
+++ tp1/src/main/java/tp1/Server.java
@@ -6,24 +6,28 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server
 {   
+    // Initialisation des valeurs importantes
     private static ServerSocket listener;
+    private static String serverIP = "";
+    private static int serverPort = 0;
+    private static Scanner clientInput = new Scanner(System.in); // Creation du Scanner pour avoir le user input
 
     /* Application Serveur*/
     public static void main( String[] args ) throws Exception { 
         // Compteur incremente a chaque connexion d'un client au serveur
-        int clientNumber = 0;
+        int clientNumber = 1; // premier client
 
-        // Adresse et port du serveur
-        String serverAddress = "127.0.0.1";
-        int serverPort = 5000;
+        // Tant que l'addresse et le port ne sont pas valides demander de nouveau
+        while(invalidInput());
 
         // Creation de la connexion pour communiquer avec les clients
         listener = new ServerSocket();
         listener.setReuseAddress(true);
-        InetAddress serverIP = InetAddress.getByName(serverAddress);
+        InetAddress serverAddress = InetAddress.getByName(serverIP);
 
         //Association de l'adresse et du port a la connexion
         listener.bind(new InetSocketAddress(serverIP, serverPort));
@@ -45,6 +49,96 @@ public class Server
             listener.close();
         }
     }
+
+    /*
+    * Demander le IP addresse et le port, puis verifier s'il sont valides
+    */
+    private static Boolean invalidInput() {   
+        // Demander au client sur quel port il veut se connecter
+        System.out.println("Please enter the IP address and the port (between 5000 and 5050) to connect to: ");
+        String input = clientInput.nextLine();
+        // Decouper le input de maniere que A.B.C.D:E -> [A, B, C, D, E]
+        String[] inputArray = input.split("[:\\.]", 6);
+
+        return(validInput(inputArray) && validIP(inputArray) && validPort(inputArray));
+    }
+
+    /*
+    * Verifier si le input addresse est valide
+    */
+    private static Boolean validInput(String[] inputArray) {
+        boolean validInput = true;
+
+        if(inputArray.length != 5) {
+            validInput = false;
+            System.out.println(" Please enter a valid input! (eg. 127.0.0.1:5001)");
+        }
+    
+        return(validInput);
+
+    }
+
+    /*
+    * Verifier si le IP addresse est valide
+    */
+    private static Boolean validIP(String[] inputArray) {
+        boolean validIP = true;
+        int ipSize = 4; // 4 octets
+        for(int i = 0; i < ipSize; i++) {
+            
+            int octet = 0;
+            
+            // Verifier si ce n'est pas des lettres
+            try {
+                octet = Integer.parseInt(inputArray[i]);
+            }
+            catch(NumberFormatException e) {
+                validIP = false;
+                System.out.println(" Please enter a valid IP number! (no letters)");
+            }
+
+            // Verifier si respecte [0, 255]
+            if(octet < 0 || octet > 255) {
+                validIP = false;
+                System.out.println(" Please enter an IP address inside bounderies! (XX.XX.XX.XX:YYYY where XX = [0, 255])");
+            }
+            
+            serverIP = inputArray[i] + '.';
+        }
+        return validIP;
+    }
+
+    /*
+    * Verifier si le port est valide
+    */
+    private static Boolean validPort(String[] inputArray) {
+
+        boolean validPort = true;
+        String portInput = inputArray[5];
+            
+            int port = 0;
+            
+            // Verifier si ce n'est pas des lettres
+            try {
+                port = Integer.parseInt(portInput);
+            }
+            catch(NumberFormatException e) {
+                validPort = false;
+                System.out.println(" Please enter a valid port number! (no letters)");
+            }
+
+            // Verifier si respecte [0, 255]
+            if(port < 5000 || port > 5050) {
+                validPort = false;
+                System.out.println(" Please enter a port number inside bounderies! (XX.XX.XX.XX:YYYY where YYYY = [5000, 5050])");
+            }
+            
+            serverPort = port;
+
+        return validPort;
+    }
+
+
     /*
     * Un thread qui se charge de traiter la demande de chaque client 
     * sur un socket particulier
